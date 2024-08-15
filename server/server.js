a8775703b6291f9410074b4bf20445002281837c
Boyan <104321637+BoyanDashev@users.noreply.github.com>
2024-08-15 18:37:23 +0300

    New changes

--- server/server.js
+++ server/server.js
@@ -3,14 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path"); // Add this line
 const app = express();
 const port = process.env.PORT || 5000;
 const User = require("./modules/module");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const allowedOrigins = require("./config/allowedOrigins");
-
-  
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -24,13 +23,11 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(cookieParser());
-
 app.use(express.json());
-
 app.use(bodyParser.json());
 
+// MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -49,8 +46,27 @@ connection.once("open", () => {
   console.log(`Connection host: ${mongoose.connection.host}`);
 });
 
-app.listen(port, '0.0.0.0', () => {
+// API Routes
+app.use("/api", authRoutes);
+
+// Serve static files from the React app's build directory in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  // All other routes should return the React app
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      (err) => {
+        if (err) {
+          res.status(500).send("Internal Server Error");
+        }
+      }
+    );
+  });
+}
+
+// Start the server
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port: ${port}`);
 });
-
-app.use("/api", authRoutes);
